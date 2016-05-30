2d658b12f8cf6510a5dadc673d73cfd59e96908d
duniv5-022 <duniv5-022@duniv5-022-PC>
2016-05-30 10:56:34 +0900

    test

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println("test");
+		System.out.println("test2");
+		
 
 	}
 
